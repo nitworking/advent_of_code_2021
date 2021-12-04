bae5af0462ded173a3ffac53adc5f939a1d2acec
Nitworking <nitworking@pnm.me>
2021-12-04 22:38:40 +0800

    Exercise 1 example with implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+pub fn count_increasing(depth_measurements: &[i32]) -> i32 {
+    use std::i32::MAX;
+
+    let mut bigger_than_previous: i32 = 0;
+
+    let mut previous: i32 = MAX;
+    for measurement in depth_measurements.iter() {
+        if measurement > &previous {
+            bigger_than_previous += 1;
+        }
+        previous = measurement.clone();
+    }
+
+    return bigger_than_previous;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_exercise() {
+        let depth_measurements = [
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263,
+        ];
+
+        assert_eq!(7, count_increasing(&depth_measurements));
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // count_increasing();
 }
