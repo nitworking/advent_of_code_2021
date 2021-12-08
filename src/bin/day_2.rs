d02e32fec4ff9c672b68497ba8b061eb629eace3
Nitworking <nitworking@pnm.me>
2021-12-08 21:17:22 +0800

    Day 2 Part 2

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -16,6 +16,26 @@ pub fn calculate_destination(course: &Vec<(String, i32)>) -> (i32, i32) {
     return (hpos, depth);
 }
 
+pub fn calculate_destination_2(course: &Vec<(String, i32)>) -> (i32, i32) {
+    let mut hpos = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    
+    for (direction, units) in course {
+        match direction.as_str() {
+            "forward" => {
+                hpos += units;
+                depth += aim * units;
+            },
+            "up" => aim -= units,
+            "down" => aim += units,
+            _ => eprintln!("Unknown direction {}", direction)
+        }
+    }
+
+    return (hpos, depth);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,6 +55,22 @@ mod tests {
 
         assert_eq!(150, location.0 * location.1);
     }
+
+    #[test]
+    fn example_exercise_part_2() {
+        let course = vec!(
+            (String::from("forward"), 5),
+            (String::from("down"), 5),
+            (String::from("forward"), 8),
+            (String::from("up"), 3),
+            (String::from("down"), 8),
+            (String::from("forward"), 2),
+        );
+
+        let location = calculate_destination_2(&course);
+
+        assert_eq!(900, location.0 * location.1);
+    }
 }
 
 
@@ -51,4 +87,7 @@ fn main() {
 
     let (hpos, depth) = calculate_destination(&course);
     println!("Multiplied: {}", hpos*depth);
+
+    let (hpos, depth) = calculate_destination_2(&course);
+    println!("Multiplied 2: {}", hpos*depth);
 }
